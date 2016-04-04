948017c4caffbb87e453e2a5e7fc568167a34681
echicken <echicken@bbs.electronicchicken.com>
2016-04-04 13:04:38 -0400

    Abandoned pngjs, jpeg-js, and sharp in favour of Jimp.  Simplifies things, and no native dependencies any longer.

--- picture.js
+++ picture.js
@@ -1,10 +1,12 @@
-var PNG = require('pngjs').PNG,
-	JPEG = require('jpeg-js'),
-	sharp = require('sharp');
+var util = require('util'),
+	EventEmitter = require('events'),
+	Jimp = require('jimp');
 
-var Picture = function (options, callback) {
+var Picture = function (data) {
 
-	var self = this, data = [], width = 0, height = 0;
+	EventEmitter.call(this);
+
+	var self = this, image = { bitmap : { data : [], width : 0, height : 0 }};
 
 	function toAF(n) {
 		return 1500 + Math.min(255, Math.max(0, n)) * 3.1372549;
@@ -12,106 +14,89 @@ var Picture = function (options, callback) {
 
 	function RGBtoYUV(r, g, b) {
 		return {
-			y : 16 + (.003906 * ((65.738 * r) + (129.057 * g) + (25.064 * b))),
-			u : 128 + (
-				.003906 * ((112.439 * r) + (-94.154 * g) + (-18.285 * b))
-			),
-			v : 128 + (
-				.003906 * ((-37.945 * r) + (-74.494 * g) + (112.439 * b))
-			)
+			y : 16+(.003906*((65.738*r)+(129.057*g)+(25.064*b))),
+			u : 128+(.003906*((112.439*r)+(-94.154*g)+(-18.285*b))),
+			v : 128+(.003906*((-37.945*r)+(-74.494*g)+(112.439*b)))
 		}
 	}
 
 	function pixelToYuvAf(index) {
-		var yuv = RGBtoYUV(data[index], data[index + 1], data[index + 2]);
+		var yuv = RGBtoYUV(
+			image.bitmap.data[index],
+			image.bitmap.data[index + 1],
+			image.bitmap.data[index + 2]
+		);
 		return { y : toAF(yuv.y), u : toAF(yuv.u), v : toAF(yuv.v) };
 	}
 
 	function pixelToRgbAf(index) {
 		return {
-			r : toAF(data[index]),
-			g : toAF(data[index + 1]),
-			b : toAF(data[index + 2])
+			r : toAF(image.bitmap.data[index]),
+			g : toAF(image.bitmap.data[index + 1]),
+			b : toAF(image.bitmap.data[index + 2])
 		};
 	}
 
-	function lineToYuvAF(y) {
+	function lineToYuvAf(y) {
 		var line = [];
-		for (var x = 0; x < width; x++) {
-			line[x] = pixelToYuvAf(((width * y + x)<<2));
+		for (var x = 0; x < image.bitmap.width; x++) {
+			line[x] = pixelToYuvAf(((image.bitmap.width * y + x)<<2));
 		}
 		return line;
 	}
 
 	function imageToYuvAf() {
 		var lines = [];
-		for (var y = 0; y < height; y++) {
-			lines[y] = lineToYuvAF(y);
+		for (var y = 0; y < image.bitmap.height; y++) {
+			lines[y] = lineToYuvAf(y);
 		}
 		return lines;
 	}
 
 	function lineToRgbAf(y) {
 		var line = [];
-		for (var x = 0; x < width; x++) {
-			line[x] = pixelToRgbAf(((width * y + x)<<2));
+		for (var x = 0; x < image.bitmap.width; x++) {
+			line[x] = pixelToRgbAf(((image.bitmap.width * y + x)<<2));
 		}
 		return line;
 	}
 
 	function imageToRgbAf() {
 		var lines = [];
-		for (var y = 0; y < height; y++) {
+		for (var y = 0; y < image.bitmap.height; y++) {
 			lines[y] = lineToRgbAf(y);
 		}
 		return lines;
 	}
 
-	function init() {
-
-		switch (options.type.toLowerCase()) {
-			case 'png':
-				var png = new PNG();
-				png.parse(
-					options.data,
-					function (err, pData) {
-						data = pData.data;
-						width = png.width;
-						height = png.height;
-						callback.call(self);
-					}
-				);
-				break;
-			case 'jpg':
-			case 'jpeg':
-				var jpeg = JPEG.decode(options.data);
-				data = jpeg.data;
-				width = jpeg.width;
-				height = jpeg.height;
-				callback.call(self);
-				break;
-			default:
-				throw 'Invalid image type.';
-				break;
-		}
-
-	}
-
 	this.__defineGetter__('YUV_AF', function () { return imageToYuvAf(); });
 	this.__defineGetter__('RGB_AF', function () { return imageToRgbAf(); });
+	this.__defineGetter__('data', function () { return image.bitmap.data; });
+	this.__defineGetter__('width', function () { return image.bitmap.width; });
+	this.__defineGetter__('height',	function () {return image.bitmap.height;});
 
-	this.scale = function (height, cb) {
-		sharp(options.data).resize(null, height).png().toBuffer(
-			function (err, data, info) {
-				options.data = data;
-				callback = cb;
-				init();
+	this.load = function (buf) {
+		Jimp.read(
+			buf,
+			function (err, img) {
+				if (err !== null) {
+					self.emit('error', err);
+				} else {
+					image = img;
+					self.emit('ready')
+				}				
 			}
 		);
 	}
 
-	init();
+	this.scale = function (height, callback) {
+		image.resize(Jimp.AUTO, height);
+		callback.call(this);
+	}
+
+	this.load(data);
 
 }
+util.inherits(Picture, EventEmitter);
 
 module.exports = Picture;
